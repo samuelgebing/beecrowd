129f16e9aae3228c325ba25291053c97b3a79dda
Samuel Ânderson Gebing <samu.gebing@gmail.com>
2026-03-17 14:54:59 -0300

    Add Hello World program in 1000.js

---
+++ bfd/1000.js
@@ -0,0 +1,7 @@
+// https://judge.beecrowd.com/en/problems/view/1000
+// Hello World!
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+console.log("Hello World!");

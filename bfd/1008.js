79ee2395111ccb95818a04d48280c3855213c37e
Samuel Ânderson Gebing <samu.gebing@gmail.com>
2026-03-17 21:53:49 -0300

    Add solution for Salary problem 1008

---
+++ bfd/1008.js
@@ -0,0 +1,14 @@
+// https://judge.beecrowd.com/en/problems/view/1008
+// Salary
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var employeeNum = parseInt(lines.shift());
+var workHours = parseInt(lines.shift());
+var hourValue = parseFloat(lines.shift());
+
+var salary = workHours * hourValue;
+
+console.log(`NUMBER = ${employeeNum}`);
+console.log(`SALARY = U$ ${salary.toFixed(2)}`);

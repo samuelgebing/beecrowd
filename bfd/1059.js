bc06fddcc516226c704be203eadbc4a11c40eb1d
Samuel Ânderson Gebing <samu.gebing@gmail.com>
2026-03-19 12:54:06 -0300

    Add script to print even numbers from 1 to 100: 1059.js

---
+++ bfd/1059.js
@@ -0,0 +1,11 @@
+// https://judge.beecrowd.com/en/problems/view/1059
+// Even numbers
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
